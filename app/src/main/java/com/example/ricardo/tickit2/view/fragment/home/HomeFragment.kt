d8b75b03b939ef28959bf30b8d878b8d776a6721
ArilexWang <383234436@qq.com>
2017-12-26 20:13:41 +0800

    recyleview

--- app/src/main/java/com/example/ricardo/tickit2/view/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/example/ricardo/tickit2/view/fragment/home/HomeFragment.kt
@@ -12,23 +12,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
 import com.example.ricardo.tickit2.R
-import com.example.ricardo.tickit2.R.id.banner
-import com.example.ricardo.tickit2.R.id.image
 import com.example.ricardo.tickit2.data.model.BannerPicture
 import com.example.ricardo.tickit2.data.network.repository.BannerPicRepository
 import com.example.ricardo.tickit2.view.advertisement.AdvertisementActivity
+import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.drawee.view.SimpleDraweeView
 import com.google.gson.Gson
 import com.youth.banner.Banner
 import com.youth.banner.BannerConfig
 import com.youth.banner.listener.OnBannerListener
+import kotlinx.android.synthetic.main.activity_setting.view.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.item_horizan_list.*
+import kotlinx.android.synthetic.main.item_horizan_list.view.*
 import java.util.ArrayList
+
+
 
 
 class HomeFragment: android.support.v4.app.Fragment(),HomeView,OnBannerListener {
 
     val presenter: HomePresenter = HomePresenter(this, BannerPicRepository.get())
+
+    val imgIDs = ArrayList<Int>()
+
+    var mInflate:LayoutInflater? = null
+
 
     private val images = ArrayList<String>()
 
@@ -36,16 +45,43 @@ class HomeFragment: android.support.v4.app.Fragment(),HomeView,OnBannerListener 
 
     @Nullable
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Fresco.initialize(context)
         val view = inflater!!.inflate(R.layout.fragment_home, null)
+        mInflate = LayoutInflater.from(context)
+        val lLayout:LinearLayout = view.findViewById(R.id.homeListHorizon);
+
+        initView(lLayout)
+
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         presenter.start()
+
     }
 
+
+    private fun initView(layout: LinearLayout){
+        imgIDs.add(R.drawable.shake)
+        imgIDs.add(R.drawable.shake)
+
+        val view:View = mInflate!!.inflate(R.layout.item_horizan_list,homeListHorizon,false)
+
+        view.home_film_icon.setImageURI("http://p04jew294.bkt.clouddn.com/avatar.jpg")
+
+        view.home_film_title.setText("title")
+
+        view.home_film_grade.setText("6.0")
+
+        layout.addView(view)
+
+
+    }
+
+
     override fun OnBannerClick(position: Int) {
+
         println(bannerPics[position].targetPath)
         val url = bannerPics[position].targetPath
         val intent = Intent(context, AdvertisementActivity::class.java)
@@ -70,7 +106,7 @@ class HomeFragment: android.support.v4.app.Fragment(),HomeView,OnBannerListener 
 
 
      override fun onError(error: Throwable) {
-
+         Toast.makeText(context,"加载图片失败",Toast.LENGTH_SHORT).show();
      }
 
 

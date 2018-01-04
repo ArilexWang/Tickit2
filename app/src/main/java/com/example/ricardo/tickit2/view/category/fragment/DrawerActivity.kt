b2c0861269c385c25b761f3bcde6cbac4dcee1b1
yuhanyin <593349417@qq.com>
2018-01-04 12:31:03 +0800

    add category button view

---
+++ app/src/main/java/com/example/ricardo/tickit2/view/category/fragment/DrawerActivity.kt
@@ -0,0 +1,57 @@
+package com.example.ricardo.tickit2.view.category.fragment
+
+import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.example.ricardo.tickit2.BuildConfig
+import com.example.ricardo.tickit2.R
+import com.example.ricardo.tickit2.base.BasePresenter
+import com.example.ricardo.tickit2.view.common.BaseActivity
+import kotlinx.android.synthetic.main.fragment_category.*
+
+/**
+ * Created by yuhanyin on 1/3/18.
+ */
+open class DrawerActivity : BaseActivity() {
+    override val presenter: BasePresenter
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    private var mDrawerToggle: ActionBarDrawerToggle? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        if (!BuildConfig.DEBUG) {
+//            Fabric.with(this, Crashlytics())
+//        }
+
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onStart() {
+        super.onStart()
+
+        mDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, 0, 0)
+        drawerLayout!!.setDrawerListener(mDrawerToggle)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setDisplayShowHomeEnabled(true)
+            actionBar.setDisplayShowTitleEnabled(true)
+            actionBar.setDisplayUseLogoEnabled(false)
+            actionBar.setHomeButtonEnabled(true)
+        }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        mDrawerToggle!!.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return mDrawerToggle!!.onOptionsItemSelected(item) || super.onOptionsItemSelected(item)
+    }
+}
---
+++ app/src/main/java/com/example/ricardo/tickit2/view/category/fragment/RecyclerViewFragment.kt
@@ -0,0 +1,63 @@
+package com.example.ricardo.tickit2.view.category.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ricardo.tickit2.R
+import com.example.ricardo.tickit2.R.id.recyclerView
+import com.github.florent37.materialviewpager.header.MaterialViewPagerHeaderDecorator
+import kotlinx.android.synthetic.main.fragment_recyclerview.*
+import java.util.ArrayList
+
+/**
+ * Created by yuhanyin on 1/4/18.
+ */
+class RecyclerViewFragment : Fragment() {
+
+//    @BindView(R.id.recyclerView)
+//    internal var mRecyclerView: RecyclerView? = null
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_recyclerview, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        ButterKnife.bind(this, view)
+
+        val items = ArrayList<Any>()
+
+        for (i in 0 until ITEM_COUNT) {
+            items.add(Any())
+        }
+
+
+        //setup materialviewpager
+
+        if (GRID_LAYOUT) {
+            recyclerView.layoutManager = GridLayoutManager(activity, 2)
+        } else {
+            recyclerView.layoutManager = LinearLayoutManager(activity)
+        }
+        recyclerView.setHasFixedSize(true)
+
+        //Use this now
+        recyclerView.addItemDecoration(MaterialViewPagerHeaderDecorator())
+        recyclerView.adapter = RecyclerViewAdapter(items)
+    }
+
+    companion object {
+
+        private val GRID_LAYOUT = false
+        private val ITEM_COUNT = 100
+
+        fun newInstance(): RecyclerViewFragment {
+            return RecyclerViewFragment()
+        }
+    }
+}

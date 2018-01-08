c9fec32e80f066daee22181b88096a1db2411ae5
ArilexWang <383234436@qq.com>
2018-01-08 15:46:36 +0800

    set show

--- app/src/main/java/com/example/ricardo/tickit2/data/network/api/showapi/SetShowApi.kt
+++ app/src/main/java/com/example/ricardo/tickit2/data/network/api/showapi/SetShowApi.kt
@@ -1,6 +1,7 @@
 package com.example.ricardo.tickit2.data.network.api.showapi
 
 import com.example.ricardo.tickit2.data.dto.BannerPicDto
+import com.example.ricardo.tickit2.data.dto.ShowDto
 import io.reactivex.Single
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -16,11 +17,12 @@ interface SetShowApi {
             @Field("studentID") studentID: String,
             @Field("password") password: String,
             @Field("showID") showID: String,
-            @Field("showName") showName: String,
-            @Field("showAvatar") showAvatar:String,
-            @Field("descriptionURL") descriptionURL: String,
+            @Field("name") showName: String,
+            @Field("avatar") showAvatar:String,
+            @Field("description") descriptionURL: String,
             @Field("showTime") showTime: String,
+            @Field("expiredTime") expiredTime: String,
             @Field("category") category: Int,
-            @Field("isOnSale") isOnDisplay: Int
-    ): Single<List<BannerPicDto>>
+            @Field("isOnSale") isOnSale: Int
+    ): Single<List<ShowDto>>
 }
